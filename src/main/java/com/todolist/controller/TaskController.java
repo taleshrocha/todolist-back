0ee613dd549e569794355e6682dbf5be7001f1a2
taleshrocha <taleshrochaz@gmail.com>
2023-03-06 10:02:23 -0300

    Delete Task

--- src/main/java/com/todolist/controller/TaskController.java
+++ src/main/java/com/todolist/controller/TaskController.java
@@ -2,6 +2,7 @@ package com.todolist.controller;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,4 +44,8 @@ public class TaskController {
 		return task;
 	}
 
+	@DeleteMapping("/task/{id}")
+	public void deleteTask(@PathVariable Long id) {
+		taskRepository.deleteById(id);
+	}
 }
