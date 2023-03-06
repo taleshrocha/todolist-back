f438b4382870cd019226a4e8116380310ca417c0
taleshrocha <taleshrochaz@gmail.com>
2023-03-06 10:30:07 -0300

    Cors config

--- src/main/java/com/todolist/TodolistApplication.java
+++ src/main/java/com/todolist/TodolistApplication.java
@@ -2,9 +2,24 @@ package com.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TodolistApplication {
+
+	@Bean
+	public WebMvcConfigurer corsConfigure() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("*");
+		}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistApplication.class, args);
